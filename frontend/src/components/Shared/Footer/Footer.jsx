24e72bc6cdcd5196b92cfb816cec4aa21de3fa29
Aftab Farhan <aftabfarhan324@gmail.com>
2025-11-20 03:39:34 +0600

    footrt

--- frontend/src/components/Shared/Footer/Footer.jsx
+++ frontend/src/components/Shared/Footer/Footer.jsx
@@ -1,11 +1,33 @@
 const Footer = () => {
   return (
-    <footer className='px-4 divide-y  text-gray-800 relative bottom-0 left-0'>
-      <div className='py-6 text-sm text-center text-gray-400'>
+    <footer className=" divide-y mt-15  text-gray-800 relative bottom-0 left-0">
+      <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
+        <nav>
+          <h6 className="footer-title">Services</h6>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Company</h6>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Legal</h6>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </nav>
+      </footer>
+      {/* <div className=' text-sm text-center text-gray-400'>
         © 2025-2026 PlantNet Inc. All rights reserved.
-      </div>
+      </div> */}
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
