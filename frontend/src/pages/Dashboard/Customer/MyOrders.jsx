ece0427a2189e3e7c4883a5b631ef9ffb2ec9d8f
Aftab Farhan <aftabfarhan324@gmail.com>
2025-12-04 14:46:13 +0600

    addes

--- frontend/src/pages/Dashboard/Customer/MyOrders.jsx
+++ frontend/src/pages/Dashboard/Customer/MyOrders.jsx
@@ -1,62 +1,83 @@
-import CustomerOrderDataRow from '../../../components/Dashboard/TableRows/CustomerOrderDataRow'
+import { useQuery } from "@tanstack/react-query";
+import CustomerOrderDataRow from "../../../components/Dashboard/TableRows/CustomerOrderDataRow";
+import useNormalAxios from "../../../hooks/useNormalAxios";
+import useAuth from "../../../hooks/useAuth";
+import LoadingSpinner from "../../../components/Shared/LoadingSpinner";
 
 const MyOrders = () => {
+  const axiosNormal = useNormalAxios();
+  const {user} = useAuth()
+  const { data: plants = [], isLoading } = useQuery({
+    queryKey: ["orderdata", user.email],
+    queryFn: async () => {
+      const res = await axiosNormal.get(`customer-order?email=${user.email}`);
+      return res?.data;
+    },
+  });
+  console.log(plants);
+
+  if (isLoading) {
+    return <LoadingSpinner></LoadingSpinner>;
+  }
   return (
     <>
-      <div className='container mx-auto px-4 sm:px-8'>
-        <div className='py-8'>
-          <div className='-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto'>
-            <div className='inline-block min-w-full shadow rounded-lg overflow-hidden'>
-              <table className='min-w-full leading-normal'>
-                <thead>
+      <div className="container mx-auto px-4 sm:px-8">
+        <div className="py-8">
+          <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+            <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+              <table className="min-w-full leading-normal">
+                <thead className="">
                   <tr>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 font-semibold py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase "
                     >
                       Image
                     </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-semibold"
                     >
                       Name
                     </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-semibold "
                     >
                       Category
                     </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-semibold"
                     >
                       Price
                     </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-semibold"
                     >
                       Quantity
                     </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-semibold"
                     >
                       Status
                     </th>
 
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-semibold"
                     >
                       Action
                     </th>
                   </tr>
                 </thead>
                 <tbody>
-                  <CustomerOrderDataRow />
+                  {
+                    plants.map(item =>  <CustomerOrderDataRow key={item._id} item={item} />)
+                  }
+                 
                 </tbody>
               </table>
             </div>
@@ -64,7 +85,7 @@ const MyOrders = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default MyOrders
+export default MyOrders;
--- frontend/src/pages/Dashboard/Seller/ManageOrders.jsx
+++ frontend/src/pages/Dashboard/Seller/ManageOrders.jsx
@@ -1,62 +1,89 @@
-import SellerOrderDataRow from '../../../components/Dashboard/TableRows/SellerOrderDataRow'
+import { useQuery } from "@tanstack/react-query";
+import SellerOrderDataRow from "../../../components/Dashboard/TableRows/SellerOrderDataRow";
+import useAuth from "../../../hooks/useAuth";
+import useNormalAxios from "../../../hooks/useNormalAxios";
 
 const ManageOrders = () => {
+  const axiosNormal = useNormalAxios();
+  const { user } = useAuth();
+  const { data: order = [], isLoading } = useQuery({
+    queryKey: ["orderdata",],
+    queryFn: async () => {
+      const res = await axiosNormal.get(`manez-order?email=${user.email}`);
+      return res?.data;
+    },
+  });
+  console.log(order);
+
+  // if (isLoading) {
+  //   return <LodingSpi
+  // }
+
   return (
     <>
-      <div className='container mx-auto px-4 sm:px-8'>
-        <div className='py-8'>
-          <div className='-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto'>
-            <div className='inline-block min-w-full shadow rounded-lg overflow-hidden'>
-              <table className='min-w-full leading-normal'>
+      <div className="container mx-auto px-4 sm:px-8">
+        <div className="py-8">
+          <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+            <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+              <table className="min-w-full leading-normal">
                 <thead>
                   <tr>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-semibold"
+                    >
+                      Image
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-semibold"
                     >
                       Name
                     </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-semibold"
                     >
                       Customer
                     </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-semibold"
                     >
                       Price
                     </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-semibold"
                     >
                       Quantity
                     </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-semibold"
                     >
                       Address
                     </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-semibold"
                     >
                       Status
                     </th>
 
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-semibold"
                     >
                       Action
                     </th>
                   </tr>
                 </thead>
                 <tbody>
-                  <SellerOrderDataRow />
+                  {
+                    order.map(item =>  <SellerOrderDataRow  key={item._id} item={item}/>)
+                  }
+                 
                 </tbody>
               </table>
             </div>
@@ -64,7 +91,7 @@ const ManageOrders = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default ManageOrders
+export default ManageOrders;
